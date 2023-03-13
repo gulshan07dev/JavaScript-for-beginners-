154aa38aab695eb8d5874832a72846cdc933db1d
Gulshan kumar <gulshanku9135@gmail.com>
2023-03-13 13:48:13 +0530

    array cmmit

---
+++ Array/arraybasics.js
@@ -0,0 +1,19 @@
+// styntax of array [array is a container that contain homogeonues or heterogeones item]
+// let arrayName = [element];
+// ex-
+
+let arr = [1, 2, 3, "hello"];
+console.log(arr);
+
+// how to acces element of the array -> using indexes [in javascript array is zero based index means - index start from 0]
+
+// ex-
+
+console.log(arr[0], arr[1]);
+
+// i also change element of the array using indexes
+
+// ex-
+
+arr[0] = 10;
+console.log(arr[0]);
---
+++ Array/arraymethods.js
@@ -0,0 +1,35 @@
+// array methods
+
+// array length -> this method return length of the array
+console.log(arr.length);
+
+// push -> this method used for add element in last of the array
+arr.push(5);
+console.log(arr);
+
+// pop -> this method used for remove element in last of the array
+arr.pop();
+console.log(arr);
+
+// shift -> this method used for remove element in begin of the array
+arr.shift();
+console.log(arr);
+
+// unshift -> this method used for add element in begin of the array
+arr.unshift(1);
+console.log(arr);
+
+// splice -> this method used for add element in specific position of the array
+arr.splice(3, 0, 10);
+console.log(arr);
+
+// slice -> this method used for return a section of the array
+console.log(arr.slice(1, 2));
+
+// reverse -> this method used for return reverse of the array
+arr.reverse();
+console.log(arr);
+
+// sort -> this method used for retun sort(ascending order)
+arr.sort();
+console.log(arr);
