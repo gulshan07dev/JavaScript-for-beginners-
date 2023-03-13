c31834c21200533c605b7100e02b541827ed365e
Gulshan kumar <gulshanku9135@gmail.com>
2023-03-13 13:50:17 +0530

    operators commit

---
+++ operators/operators.js
@@ -0,0 +1,48 @@
+// Assignment operator -> used for assign a value in valiables
+let highScore = 300
+console.log(highScore)
+
+let pwSkillsCoursePrice = 3500
+console.log(pwSkillsCoursePrice);
+
+pwSkillsCoursePrice = 2000
+console.log(pwSkillsCoursePrice);
+
+let userName = "hitesh"
+
+let pwCoursePrice = 200
+let gstOnCourses = 36
+let finalPurchaseAmount = pwCoursePrice + gstOnCourses
+//console.log("Final amount to be paid: ", finalPurchaseAmount);
+
+let pwDsaCourse = 500
+//console.log("Final price of DSA course: ", pwDsaCourse / gstOnCourses);
+
+
+// console.log("Answer is: ", 5 % 2);
+// console.log("Answer2 is: ", 5 / 2);
+// console.log("Answer3 is: ", 3 ** 3);
+
+
+let hiteshHighScore = 200
+let rajuHighScore = 300
+
+console.log(hiteshHighScore != rajuHighScore);
+
+
+let value1 = true
+let value2 = false
+let value3 = true
+let value4 = false
+
+console.log(value1 && value3);
+
+
+let isLoggedIn = true
+let cardDetails = true
+
+// || operator
+let gmailAccount = true
+let emailAccount = false
+let githubLogin = false
+console.log(gmailAccount || emailAccount);
