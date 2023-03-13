ee5852c1eb30b1f7127edcd554ca273486eb9a79
Gulshan kumar <gulshanku9135@gmail.com>
2023-03-13 13:51:10 +0530

    datatypes commit

---
+++ datatypes/datatypes.js
@@ -0,0 +1,17 @@
+// Values and Datatypes
+
+// types of datatypes
+
+// 1 - primitive Data Types
+
+// - number: 2, 3, 4, 4.5, 24
+// - String: "PW Skills" or 'PW SKills'
+// - Boolean: true, false
+// - null -> null means nothing
+// - undefinedv -> variable declared but value not assigned
+
+// 2 - Non Primitive Data Types
+//ex - Array, objects
+ 
+// array: []
+let names = ["hitesh", "sushant", "rohit", "Kaif", 2, 3, false, "hitesh"]
